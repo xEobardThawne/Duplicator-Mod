a6278f19393617ba19bb95234dcb44764efc1f63
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-08-06 12:30:39 -0500

    Upload Mod Template
    Just uploading the mod template!

---
+++ src/client/java/xeobardthawne/duplicatormod/DuplicatorModClient.java
@@ -0,0 +1,10 @@
+package xeobardthawne.duplicatormod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DuplicatorModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
